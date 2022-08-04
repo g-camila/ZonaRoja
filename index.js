798b243130a6015b8993d6d074413feeda981419
g-camila <camilagiron154@gmail.com>
2022-08-04 19:32:49 -0300

    minor changes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Initialize and add the map
+let map;
+
 function initMap() {
     const Cordoba = { lat: -31.420083, lng:-64.188776};
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 12,
       center: Cordoba,
     });
-    const marker = new google.maps.Marker({
-      position: Cordoba,
-      map: map,
-    });
+
+    map.data.addGeoJson("barrios2.geojson");
   }
   
   window.initMap = initMap;
