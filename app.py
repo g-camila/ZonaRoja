64d28eebb12ef136d493bd4fba9b91a59b91cf59
g-camila <camilagiron154@gmail.com>
2022-12-29 16:18:03 -0300

    added some comments and changed a sql table

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
 
 from flask import Flask, flash, redirect, render_template, request
-#from urllib import request
-#import pyodbc
 import sqlalchemy as sqla
 import urllib
 import pandas as pd
@@ -11,7 +9,7 @@ import pandas as pd
 serverdb = 'eikonweb.eastus.cloudapp.azure.com' 
 database = 'registros' 
 username = 'sa' 
-password = 'Dycsa0000' 
+password= os.environ.get("sqlkey")
 cnxn= urllib.parse.quote_plus('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+serverdb+';DATABASE='+database+';UID='+username+';PWD='+ password)
 engine = sqla.create_engine('mssql+pyodbc:///?odbc_connect={}'.format(cnxn))
 
@@ -33,9 +31,6 @@ def after_request(response):
 
 @app.route("/", methods=["GET", "POST"])
 def map_2():
-    #big_query = "SELECT * FROM casos"
-    #casos = pd.read_sql(big_query, engine)
-
     d_query = "SELECT * FROM delitos"
     delitos = pd.read_sql(d_query, engine)
 
@@ -66,33 +61,6 @@ def map_2():
     joined_tables=pd.read_sql(yy, engine)
     ammount = joined_tables.shape[0]
 
-    return render_template("map_2.html", casos=joined_tables, delitos=delitos, periodo=periodo, riesgo=jriesgo, am_casos=ammount)#, joined_t=joined_tables)
-    #return redirect(request.referrer)
+    return render_template("map_2.html", casos=joined_tables, delitos=delitos, periodo=periodo, riesgo=jriesgo, am_casos=ammount)
 if __name__=="__main__":
-    #from waitress import serve
-    #serve(app, host="0.0.0.0", port=8080)
     app.run(host="0.0.0.0", port=5000)
-
-#@app.route("/insert_sql", methods=["GET", "POST"])
-#def insert_sql():
-    #Request.
-    #if request.method == "POST":
-    #sex=request.args.get("sexo")
-    #fecha=request.args.get("dia")
-    #crimen=request.args.get("crimen")
-    #tiempo=request.args.get("horario")
-    #loc=request.args.get("coordenadas")
-    #idbarr=request.args.get("barrio_id")
-
-    #insertar = "INSERT INTO casos (lugar, fecha, periodo_id, delito_id, sexo, id_barrio) VALUES ('{}', '{}','{}', '{}','{}', '{}')".format(loc, fecha, tiempo, crimen, sex, idbarr)
-    #engine.execute(insertar)
-
-
-    #q = "SELECT * FROM casos"
-    #datos = pd.read_sql(q, engine)
-
-    #jdatos=datos.to_json()
-    #print(jdatos)
-
-    #return render_template("map_2.html")
-
